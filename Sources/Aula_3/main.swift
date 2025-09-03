dd00a6b46ec0e53bf4d612432e2c233d160d9fe5
julialonghi <116280754+julialonghi@users.noreply.github.com>
2025-09-03 18:33:24 +0000

    aula 3 alterada

--- Sources/Aula_3/main.swift
+++ Sources/Aula_3/main.swift
@@ -1,3 +1,39 @@
 import Foundation
 
-print("Aula 3 de Swift!!")
+class Carro {
+    private(set) var marca: String
+    private(set) var modelo: String
+
+    init(marca: String, modelo: String) {
+        self.marca = marca
+        self.modelo = modelo
+    }
+
+    public func acelerar() {
+        print("O carro está acelerando!")
+    }
+
+    public func frear() {
+        print("O carro está freando!")
+    }
+
+    private func setMarca(marca: String) {
+        self.marca = marca
+    }
+
+    public func alterarMarca(senha: Int, novaMarca: String) {
+        if senha == 123 {
+            self.setMarca(marca: novaMarca)
+        }
+    }
+}
+
+let carro1 = Carro(marca: "Fiat", modelo: "500")
+print(carro1.marca)
+print(carro1.modelo)
+
+//carro1.setMarca(marca: "fiat")
+//print(carro1.marca)
+
+carro1.alterarMarca(senha: 123, novaMarca: "aa")
+print(carro1.marca)
